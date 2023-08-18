23d99267eca575dbf1f035e34f75551e8160c715
Cury25 <curyfreitas@gmail.com>
2023-08-17 23:21:37 -0300

    final project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,67 @@
 import { Request, Response } from "express";
+import { pet } from '../models/pet';
 import { send } from "process";
+import {createMenuObject} from '../helpers/createMenuObject';
 
 export const home =(req: Request, res: Response) => {
-    res.send('home no controller');
-
-    // res.render('pages/page');
+       let list = pet.getAll();
+      
+         res.render('pages/page', {
+         menu: createMenuObject('all'),
+         banner: {
+            title:  'Todos os animais',
+            backgrond: 'allanimals.jpg'
+         },
+         list
+      
+      });
 
 }
 export const dogs =(req: Request, res: Response) => {
+    let list = pet.getFromType('dog');
 
-    // res.render('pages/page');
+   res.render('pages/page', {
+      menu: createMenuObject('dog'),
+      banner: {
+         title:  'Cachorros',
+         backgrond: 'banner_dog.jpg'
+      },
+
+      list
+   
+});
+
 }
-export const cats =(req: Request, res: Response) => {
 
-    // res.render('pages/page');
+export const cats =(req: Request, res: Response) => {
+    let list = pet.getFromType('cat')
+
+   res.render('pages/page', {
+      menu: createMenuObject('cat'),
+      banner: {
+         title:  'Gatos',
+         backgrond: 'banner_cat.jpg'
+
+      },
+
+      list
+   
+   });
+
 }
 export const fishes =(req: Request, res: Response) => {
+     let list = pet.getFromType('fish');
 
-    // res.render('pages/page');
+   res.render('pages/page', {
+      menu: createMenuObject('fish'),
+      banner: {
+         title:  'Peixes',
+         backgrond: 'banner_fish.jpg'
+
+      },
+      
+      list
+   
+   });
+
 }
